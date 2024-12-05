935d1ec2f6068ca9ff481fa16ec41b8f628923e2
LEFEVRE <lefevrejulien@live.fr>
2024-12-05 17:52:18 +0100

    first

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'; 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
